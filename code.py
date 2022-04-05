12d145f82305d2c00e6a1fcf4ee5fa4eba42d029
Adam Kelly <adam@cthulahoops.org>
2022-04-05 18:31:33 +0100

    Delete all commented code.

--- code.py
+++ code.py
@@ -5,19 +5,8 @@ import displayio
 from adafruit_magtag.magtag import MagTag
 from rainbowio import colorwheel
 
-# import time
-# import board
-# import digitalio
-# import adafruit_lis3dh
 
-# # def acc_to_color(value):
-# #     return int(abs(value) / 10 * 256)
-
-# # i2c = board.I2C()
-# # int1 = digitalio.DigitalInOut(board.ACCELEROMETER_INTERRUPT)  # Set this to the correct pin for the interrupt!
-# # lis3dh = adafruit_lis3dh.LIS3DH_I2C(i2c, int1=int1, address=0x19)
-
-# # lis3dh.range = adafruit_lis3dh.RANGE_2_G
+print("Version 1")
 
 magtag = MagTag()
 display = magtag.display
@@ -61,11 +50,6 @@ magtag.graphics.set_background(0xffffff)
 display.show(display_things)
 display_things.append(tilegrid)
 
-# time.sleep(display.time_to_refresh)
-# time.sleep(display.time_to_refresh)
-# display.refresh()
-# time.sleep(display.time_to_refresh)
-
 magtag.set_text("Hello!\nSecond.")
 
 
@@ -81,32 +65,3 @@ while True:
         pixels[i] = colorwheel((t + i * 8) & 255)
 
     time.sleep(0.05)
-
-
-# # # lis3dh.set_tap(2, 60)
-
-# # while True:
-# #     # if lis3dh.tapped:
-# #     #     magtag.peripherals.play_tone(400, 0.1)
-# #     #     while lis3dh.tapped:
-# #     #         time.sleep(0.1)
-# #     # else:
-# #     x, y, z = lis3dh.acceleration
-# #     magtag.peripherals.neopixels.fill((acc_to_color(x), acc_to_color(y), acc_to_color(z)))
-
-# #     time.sleep(0.1)
-
-# #     # if light_level < 1000:
-# #     #     magtag.peripherals.neopixels.fill((255, 55, 255))
-# #     # else:
-# #     #     magtag.peripherals.neopixel_disable = True
-
-
-
-# # button_colors = ((255, 0, 0), (255, 150, 0), (0, 255, 255), (180, 0, 255))
-
-# # magtag.peripherals.neopixel_disable = False
-# # magtag.peripherals.neopixels.fill((255,55,255))
-
-
-# # time.sleep(10)
