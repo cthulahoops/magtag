2c9a198bbd10af0710926ce8b36c3f830cf2cce6
Adam Kelly <adam@cthulahoops.org>
2022-05-20 11:02:26 +0100

    State of things just before clean-up

--- code.py
+++ code.py
@@ -9,6 +9,8 @@ from adafruit_magtag.peripherals import Peripherals
 from adafruit_magtag.network import Network
 from rainbowio import colorwheel
 
+import secrets
+
 from adafruit_bitmap_font import bitmap_font
 from adafruit_display_text.bitmap_label import Label
 
@@ -19,31 +21,14 @@ LIGHT_GREY = 0x999999
 DARK_GREY = 0x666666
 BLACK = 0x000000
 
+
 async def main():
     print("Woken: ", alarm.wake_alarm)
     display = board.DISPLAY
     peripherals = Peripherals()
+    network = Network()
 
     refresh_required = start_screen_refresh(display)
-
-    screen = displayio.Group()
-    calendar_screen = displayio.Group()
-    battery_screen = displayio.Group()
-
-    screen.append(background(display, WHITE))
-    screen.append(calendar_screen)
-
-    # palette = displayio.Palette(4)
-    # palette[3] = BLACK
-    # palette[2] = DARK_GREY
-    # palette[1] = WHITE_GREY
-    # palette[0] = WHITE
-
-    # tilegrid = displayio.TileGrid(
-    #     bitmap=ring_bitmap(), pixel_shader=palette, x=0, y=0, width=6, height=3
-    # )
-
-    # display_things.append(tilegrid)
 
     if isinstance(alarm.wake_alarm, alarm.pin.PinAlarm):
         calendar_text = sleep_storage.read_string(0)
@@ -51,71 +36,44 @@ async def main():
         try:
             peripherals.neopixel_disable = False
             peripherals.neopixels.fill(0xff00ff)
-            calendar_text = fetch_calendar()
+            calendar_text = fetch_calendar(network)
         except RuntimeError:
             calendar_text = "Could not fetch calendar"
         finally:
             peripherals.neopixel_disable = True
 
-    # calendar_text = calendar
-    print(len(calendar_text))
+        refresh_required.set()
 
-#    calendar_text = "Today: Minimal viable code!"
-#    calendar_text = '\n'.join(f"{entry['time']}   {entry['summary']}" for entry in calendar)
-#    print(calendar)
+    print("Calendar length: ", len(calendar_text))
+    print("Creating UI:", time.monotonic())
+    screen = create_ui(display, calendar_text, peripherals.battery, ["X", "Calendar", "Battery", "Rainbows"])
+    print("UI done!", time.monotonic())
 
-    calendar_label = Label(
-        font,
-        anchor_point=(0.0, 0.0),
-        text=calendar_text,
-        color=BLACK,
-        padding_top=3,
-        padding_bottom=3,
-        padding_left=3,
-        padding_right=3,
-        line_spacing=0.9,
-        background_color=WHITE,
-        anchored_position=(5, 5)
-    )
-    calendar_screen.append(calendar_label)
-
-    for label in button_labels(display, ["O", "Calendar", "Battery", "Rainbows"]):
-        screen.append(label)
-
-    battery_label = Label(
-        font,
-        anchor_point=(0.0, 0.0),
-        text=f"Battery: {peripherals.battery} V",
-        color=BLACK,
-        padding_top=3,
-        padding_bottom=3,
-        padding_left=3,
-        padding_right=3,
-        line_spacing=0.9,
-        background_color=WHITE,
-        anchored_position=(5, 5)
-    )
-    battery_screen.append(battery_label)
-
-    display.show(screen)
-    refresh_required.set()
-
-    sleep_time = time.monotonic() + 10
+    sleep_time = time.monotonic() + 3600
 
     while time.monotonic() < sleep_time:
-        if peripherals.button_b_pressed:
+        if peripherals.button_a_pressed:
+            print("Button A")
+            network.connect()
+            print("Connected")
+            response = network._wifi.requests.post(
+                "http://wren.local:8123/api/services/switch/toggle",
+                headers={"Authorization": "Bearer " + secrets.key},
+                json={"entity_id": "switch.tasmota"})
+            # sleep_time = time.monotonic() + 10
+            print("B is pressed")
+            await asyncio.sleep(2.0)
+#            refresh_required.set()
+
+        elif peripherals.button_b_pressed:
             sleep_time = time.monotonic() + 10
-            print("A is pressed")
-            if screen[1] != calendar_screen:
-                screen[1] = calendar_screen
+            print("B is pressed")
             refresh_required.set()
 
         elif peripherals.button_c_pressed:
             sleep_time = time.monotonic() + 10
-            print("B is pressed")
-            battery_label.text = f"Battery: {peripherals.battery} V"
-            if screen[1] != battery_screen:
-                screen[1] = battery_screen
+            print("C is pressed")
+            calendar.text = f"Battery: {peripherals.battery} V"
             refresh_required.set()
 
         elif peripherals.button_d_pressed:
@@ -128,9 +86,66 @@ async def main():
 
     sleep_storage.store_string(0, calendar_text)
 
+    print("Ready to sleep!")
+
     time_alarm = alarm.time.TimeAlarm(monotonic_time=time.monotonic() + 120)
-    pin_alarm = alarm.pin.PinAlarm(pin=board.BUTTON_A, value=False, pull=True)
-    alarm.exit_and_deep_sleep_until_alarms(time_alarm, pin_alarm)
+
+    pin_alarms = [
+        alarm.pin.PinAlarm(pin=button, value=False, pull=True)
+        for button in [board.BUTTON_A, board.BUTTON_B]
+    ]
+    alarm.exit_and_deep_sleep_until_alarms(time_alarm, *pin_alarms)
+
+
+def first_n_lines(text, n):
+    return '\n'.join(text.split('\n')[:n])
+
+def create_ui(display, calendar_text, battery_voltage, button_label_texts):
+    screen = displayio.Group()
+    calendar_screen = displayio.Group()
+    battery_screen = displayio.Group()
+
+    screen = displayio.Group()
+    calendar_screen = displayio.Group()
+    battery_screen = displayio.Group()
+
+    screen.append(background(display, WHITE))
+    screen.append(calendar_screen)
+
+    calendar_label = Label(
+        font,
+        anchor_point=(0.0, 0.0),
+        text=first_n_lines(calendar_text, 5),
+        color=BLACK,
+        padding_top=3,
+        padding_bottom=3,
+        padding_left=3,
+        padding_right=3,
+        line_spacing=0.9,
+        background_color=WHITE,
+        anchored_position=(0, 0)
+    )
+    calendar_screen.append(calendar_label)
+
+    for label in button_labels(display, button_label_texts):
+        screen.append(label)
+
+    battery_label = Label(
+        font,
+        anchor_point=(0.0, 0.0),
+        text=f"Battery: {battery_voltage} V",
+        color=BLACK,
+        padding_top=3,
+        padding_bottom=3,
+        padding_left=3,
+        padding_right=3,
+        line_spacing=0.9,
+        background_color=WHITE,
+        anchored_position=(5, 5)
+    )
+    battery_screen.append(battery_label)
+    display.show(screen)
+    return screen
 
 def button_labels(display, label_texts):
     labels = []
@@ -152,9 +167,9 @@ def button_labels(display, label_texts):
     return labels
 
 
-def fetch_calendar():
-    network = Network()
-    return network.fetch("http://192.168.0.17:5000/").text
+def fetch_calendar(network):
+    network.connect()
+    return network.fetch("http://192.168.0.32:5000/").text
 
 def start_screen_refresh(display):
     refresh_required = asyncio.Event()
@@ -162,12 +177,19 @@ def start_screen_refresh(display):
     return refresh_required
 
 async def screen_refresher(display, refresh_required):
+    print("Screen refresher starting.")
     while True:
         await refresh_required.wait()
-        print("Waiting until I can refresh the screen.")
-        await asyncio.sleep(display.time_to_refresh + 0.1)
+        await refresh_screen_when_ready(display)
         refresh_required.clear()
+
+
+async def refresh_screen_when_ready(display):
+        print("Waiting until I can refresh the screen.")
+        while display.time_to_refresh > 0:
+            await asyncio.sleep(display.time_to_refresh)
         print(display.time_to_refresh, display.busy)
+        await asyncio.sleep(1.0)
         display.refresh()
 
 def in_ring(center, x, y, radius=20, thickness=1):
