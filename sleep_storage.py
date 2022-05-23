eb524691f69d94a8f4e909eebf62bcae23cd5fbe
Adam Kelly <adam@cthulahoops.org>
2022-05-23 17:16:38 +0100

    Use two bytes for string length so I can store longer calendar lengths.

--- sleep_storage.py
+++ sleep_storage.py
@@ -1,11 +1,18 @@
 import alarm
+import struct
+
+MAX_LENGTH = 2048
 
 def store_string(offset, item_to_save):
     text_bytes = item_to_save.encode('utf-8')
-    alarm.sleep_memory[offset] = len(text_bytes)
-    alarm.sleep_memory[offset+1:offset+1+len(text_bytes)] = text_bytes
+    text_length = len(text_bytes)
+    assert text_length < MAX_LENGTH
+    alarm.sleep_memory[offset:offset+2] = struct.pack('H', text_length)
+    data_offset = offset + 2
+    alarm.sleep_memory[data_offset:data_offset+len(text_bytes)] = text_bytes
 
 def read_string(offset):
-    text_length = alarm.sleep_memory[offset]
-    assert text_length < 256
-    return alarm.sleep_memory[offset+1:1+offset+text_length].decode('utf-8')
+    text_length, = struct.unpack('H', alarm.sleep_memory[offset:offset+2])
+    assert text_length < MAX_LENGTH
+    data_offset = offset + 2
+    return alarm.sleep_memory[data_offset:data_offset+text_length].decode('utf-8')
